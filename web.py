3501a5e290ab095c2407bd6d00965f52f515ae88
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-05-16 11:19:27 +0530

    Update web.py

--- web.py
+++ web.py
@@ -25,9 +25,7 @@ async def ping_server(url, sleep_time):
     while True:
         await asyncio.sleep(sleep_time)
         try:
-            async with ClientSession(
-                timeout=ClientTimeout(total=10)
-            ) as session:
+            async with ClientSession(timeout=ClientTimeout(total=10)) as session:
                 async with session.get(url) as resp:
                     print("Pinged server with response:", resp.status)
         except TimeoutError:
