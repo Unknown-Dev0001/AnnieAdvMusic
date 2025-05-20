e9d8cfc32e77a69dfd878759ba4632fbdb83fb7d
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-05-20 11:27:24 +0530

    Add files via upload

--- ANNIEMUSIC/plugins/bot/start.py
+++ ANNIEMUSIC/plugins/bot/start.py
@@ -1,9 +1,15 @@
 import asyncio
 import random
 import time
+
 from pyrogram import filters
 from pyrogram.enums import ChatType
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+    Message,
+)
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
@@ -23,14 +29,36 @@ from ANNIEMUSIC.utils.database import (
 )
 from ANNIEMUSIC.utils.decorators.language import LanguageStart
 from ANNIEMUSIC.utils.formatters import get_readable_time
-from ANNIEMUSIC.utils.inline.start import private_panel, start_panel
 from ANNIEMUSIC.utils.inline.help import first_page
-from config import BANNED_USERS, AYUV, HELP_IMG_URL, START_VIDS, STICKERS
+from ANNIEMUSIC.utils.inline.start import private_panel, start_panel
+from config import AYUV, BANNED_USERS, START_IMG_URL
 from strings import get_string
+
+ANNIE_VID = [
+    "https://telegra.ph/file/9b7e1b820c72a14d90be7.mp4",
+    "https://telegra.ph/file/a4d90b0cb759b67d68644.mp4",
+    "https://telegra.ph/file/72f349b1386d6d9374a38.mp4",
+    "https://telegra.ph/file/2b75449612172a96d4599.mp4",
+    "https://telegra.ph/file/b3ac2d77205d5ded860de.mp4",
+    "https://telegra.ph/file/58ae4ac86ef70dc8c8f6a.mp4",
+    "https://telegra.ph/file/c6c1ac9aee4192a8a3747.mp4",
+    "https://telegra.ph/file/55c840c8eba0555318f0d.mp4",
+    "https://telegra.ph/file/e97715885d0a0cfbddaaa.mp4",
+    "https://telegra.ph/file/943bb99829ec526c3f99a.mp4",
+]
+
+STICKERS = [
+    "CAACAgUAAx0Cd6nKUAACASBl_rnalOle6g7qS-ry-aZ1ZpVEnwACgg8AAizLEFfI5wfykoCR4h4E",
+    "CAACAgUAAx0CfL_LsAACCSRl_oru7uW8WAt3-L1pYQWe_1mxawACQw8AAj78MVeb3v2OFvEnNB4E",
+    "CAACAgEAAx0Cd6nKUAACATVl_rtAi9KCVQf8vcUC4FMDUfLP8wACHQEAAlEpDTnhphyRDaTrPR4E",
+    "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ",
+]
+
 
 async def delete_sticker_after_delay(message, delay):
     await asyncio.sleep(delay)
     await message.delete()
+
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
@@ -41,7 +69,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = first_page(_)
             await message.reply_photo(
-                photo=HELP_IMG_URL,
+                photo=START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -97,9 +125,16 @@ async def start_pm(client, message: Message, _):
         served_users = len(await get_served_users())
         UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_video(
-            random.choice(START_VIDS),
+            random.choice(ANNIE_VID),
             caption=random.choice(AYUV).format(
-                message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats
+                message.from_user.mention,
+                app.mention,
+                UP,
+                DISK,
+                CPU,
+                RAM,
+                served_users,
+                served_chats,
             ),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -109,17 +144,19 @@ async def start_pm(client, message: Message, _):
                 text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
 
+
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_video(
-        random.choice(START_VIDS),
+        random.choice(ANNIE_VID),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
+
 
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
@@ -149,7 +186,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_video(
-                    random.choice(START_VIDS),
+                    random.choice(ANNIE_VID),
                     caption=_["start_3"].format(
                         message.from_user.mention,
                         app.mention,
