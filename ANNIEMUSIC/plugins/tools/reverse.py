913b45cc3e548b4c589f3ac540079cdca0c27323
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-05-22 15:33:41 +0530

    Update reverse.py

--- ANNIEMUSIC/plugins/tools/reverse.py
+++ ANNIEMUSIC/plugins/tools/reverse.py
@@ -12,7 +12,7 @@ from pyrogram.types import (
 from pyrogram import Client, filters
 
 
-from SHUKLAMUSIC import app
+from ANNIEMUSIC import app
 
 ENDPOINT = "https://sasta-api.vercel.app/googleImageSearch"
 httpx_client = httpx.AsyncClient(timeout=60)
