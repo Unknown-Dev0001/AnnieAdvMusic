7a53d6ba18a5e71fe599bc5eeb827921ce6fb0ae
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-05-29 18:29:06 +0530

    Update mongo.py

--- ANNIEMUSIC/core/mongo.py
+++ ANNIEMUSIC/core/mongo.py
@@ -2,13 +2,13 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_DB_URI
 from ANNIEMUSIC.logging import LOGGER
 
-LOGGER(__name__).info("Connecting to your Mongo Database...")
+LOGGER(__name__).info("Cᴏɴɴᴇᴄᴛɪɴɢ ᴛᴏ ʏᴏᴜʀ Dᴀᴛᴀʙᴀsᴇ...")
 
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Annie  # Replace 'Annie' if your DB has a different name
     cardsdb = mongodb.cards        # Declare collection here if used frequently
-    LOGGER(__name__).info("Connected to your Mongo Database.")
+    LOGGER(__name__).info("Cᴏɴɴᴇᴄᴛᴇᴅ ᴛᴏ ʏᴏᴜʀ Dᴀᴛᴀʙᴀsᴇ.✅")
 except Exception as e:
-    LOGGER(__name__).error(f"Failed to connect to your Mongo Database: {e}")
+    LOGGER(__name__).error(f"Failed to connect to your Database: {e}")
     exit()
