133c87e9224423849dd547f30f463336da477754
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-06-09 18:14:15 +0530

    Update downloadrepo.py

--- ANNIEMUSIC/plugins/misc/downloadrepo.py
+++ ANNIEMUSIC/plugins/misc/downloadrepo.py
@@ -1,15 +1,18 @@
-from pyrogram import Client, filters
-import git
-import shutil
 import os
-from ANNIEMUSIC import app
+import shutil
 
+import git
+from pyrogram import filters
+
+from ANNIEMUSIC import app
 
 
 @app.on_message(filters.command(["downloadrepo"]))
 def download_repo(_, message):
     if len(message.command) != 2:
-        message.reply_text("Please provide the GitHub repository URL after the command. Example: /downloadrepo repository URL ")
+        message.reply_text(
+            "ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇ ɢɪᴛʜᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ ᴜʀʟ ᴀғᴛᴇʀ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ. ᴇxᴀᴍᴘʟᴇ: /downloadrepo Repo URL "
+        )
         return
 
     repo_url = message.command[1]
@@ -20,24 +23,47 @@ def download_repo(_, message):
             message.reply_document(zip_file)
         os.remove(zip_path)
     else:
-        message.reply_text("Unable to download the specified GitHub repository.")
+        message.reply_text("ᴜɴᴀʙʟᴇ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ sᴘᴇᴄɪғɪᴇᴅ ɢɪᴛʜᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ.")
 
 
 def download_and_zip_repo(repo_url):
     try:
         repo_name = repo_url.split("/")[-1].replace(".git", "")
         repo_path = f"{repo_name}"
-        
+
         # Clone the repository
         repo = git.Repo.clone_from(repo_url, repo_path)
-        
+
         # Create a zip file of the repository
-        shutil.make_archive(repo_path, 'zip', repo_path)
+        shutil.make_archive(repo_path, "zip", repo_path)
 
         return f"{repo_path}.zip"
     except Exception as e:
-        print(f"Error downloading and zipping GitHub repository: {e}")
+        print(f"ᴇʀʀᴏʀ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴀɴᴅ ᴢɪᴘᴘɪɴɢ ɢɪᴛʜᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ: {e}")
         return None
     finally:
         if os.path.exists(repo_path):
             shutil.rmtree(repo_path)
+
+
+__MODULE__ = "Rᴇᴘᴏ"
+__HELP__ = """
+## Cᴏᴍᴍᴀɴᴅs Hᴇᴘ
+
+### 1. /ᴅᴏᴡɴᴏᴀᴅʀᴇᴘᴏ
+**Dᴇsᴄʀɪᴘᴛɪᴏɴ:**
+Dᴏᴡɴᴏᴀᴅ ᴀɴᴅ ʀᴇᴛʀɪᴇᴠᴇ ғɪᴇs ғʀᴏᴍ ᴀ GɪᴛHᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ.
+
+**Usᴀɢᴇ:**
+/ᴅᴏᴡɴᴏᴀᴅʀᴇᴘᴏ [Rᴇᴘᴏ_URL]
+
+**Dᴇᴛᴀɪs:**
+- Cᴏɴᴇs ᴛʜᴇ sᴘᴇᴄɪғɪᴇᴅ GɪᴛHᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ.
+- Cʀᴇᴀᴛᴇs ᴀ ᴢɪᴘ ғɪᴇ ᴏғ ᴛʜᴇ ʀᴇᴘᴏsɪᴛᴏʀʏ.
+- Sᴇɴᴅs ᴛʜᴇ ᴢɪᴘ ғɪᴇ ʙᴀᴄᴋ ᴀs ᴀ ᴅᴏᴄᴜᴍᴇɴᴛ.
+- Iғ ᴛʜᴇ ᴅᴏᴡɴᴏᴀᴅ ғᴀɪs, ᴀɴ ᴇʀʀᴏʀ ᴍᴇssᴀɢᴇ ᴡɪ ʙᴇ ᴅɪsᴘᴀʏᴇᴅ.
+
+**Exᴀᴍᴘᴇs:**
+- `/ᴅᴏᴡɴᴏᴀᴅʀᴇᴘᴏ ʜᴛᴛᴘs://ɢɪᴛʜᴜʙ.ᴄᴏᴍ/ᴜsᴇʀɴᴀᴍᴇ/ʀᴇᴘᴏsɪᴛᴏʀʏ`
+
+"""
