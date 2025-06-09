874c9789bfa5ab3f6bc4b10f38f8a94f143b2606
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-06-09 19:03:21 +0530

    Update Zᴏᴍʙɪᴇs.py

--- ANNIEMUSIC/plugins/Manager/Zᴏᴍʙɪᴇs.py
+++ ANNIEMUSIC/plugins/Manager/Zᴏᴍʙɪᴇs.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import FloodWait
 from ANNIEMUSIC import app
-from utils.permissions import adminsOnly
+from ANNIEMUSIC.utils.permissions import adminsOnly
 
 chatQueue = []
 
