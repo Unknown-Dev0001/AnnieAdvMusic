39629a28ca056659595a26c3d4d4677dd713268c
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-06-12 14:39:19 +0530

    Update thumbnails.py

--- ANNIEMUSIC/utils/thumbnails.py
+++ ANNIEMUSIC/utils/thumbnails.py
@@ -131,3 +131,18 @@ async def get_thumb(videoid: str) -> str:
 
     bg.save(cache_path)
     return cache_path
+
+
+# ==== New Func ==== #
+async def gen_qthumb(vidid):
+    try:
+        query = f"https://www.youtube.com/watch?v={vidid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return YOUTUBE_IMG_URL
+
+
+from youtubesearchpython.__future__ import VideosSearch
